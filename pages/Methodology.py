6e6fc6a6b5a257952c9c46fd4a1e21915458638b
crystalwsd <wongsd76@gmail.com>
2024-11-01 15:58:20 +0800

    commit

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -14,7 +14,7 @@ st.set_page_config(
 if not ut.check_password():  
     st.stop()
 
-st.title("Methodlogy")
+st.title("Methodology")
 st.write("""Below show the detailed methodology in flow chart. Data information used in this project are mocked up, although the format of evaluation plan is taken from the
         Official-Closed evaluation plan document of old tender published.""")
 
@@ -33,3 +33,19 @@ with st.expander("Evaluate Proposal"):
 with st.expander("Evaluate Shortlisted Proposals"):
     st.image(os.path.join(vl.IMAGELOCATION, "Slide12.JPG"))
 
+with st.expander("Screenshots of Application"):
+    st.write("Document Upload Screenshots: ")
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Document Upload-Tender Evaluation File.jpg"))
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Document Upload-Tender Proposal Files.jpg"))
+
+    st.write("Evaluate Tender Proposal Screenshots: ")
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Evaluate Tender Proposal-View JSON Format.jpg"))
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Evaluate Tender Proposal-Evaluate proposal by JSON file.jpg"))
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Evaluate Tender Proposal-Save Shortlist Proposal.jpg"))
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Evaluate Tender Proposal-Chatbot.jpg"))
+
+    st.write("Evaluate Shortlisted Proposals Screenshots: ")
+    st.write("Default Landing Page:")
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Shortlisted Tender Proposal Evaluation-Landing Page.jpg"))
+    st.write("Final Evaluation of Shortlisted Proposals :")
+    st.image(os.path.join(vl.SCREENSHOTLOCATION, "Shortlisted Tender Proposal Evaluation-Final Results.jpg"))
